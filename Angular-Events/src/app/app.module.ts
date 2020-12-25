52c6af124ecc43d8f9b61b0ab57120ce0cdd564f
paulgureghian <paulgureghian@att.net>
2020-12-25 15:33:21 -0800

    First commit

---
+++ Angular-Events/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    EventsAppComponent
+  ],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
