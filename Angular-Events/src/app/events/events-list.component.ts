a5b6280f7fa9142d2b084b79417736758ddab3b3
paulgureghian <paulgureghian@att.net>
2020-12-30 15:07:07 -0800

    Added events folder

---
+++ Angular-Events/src/app/events/events-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'events-list',
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr />
+    <event-thumbnail [event]="event1"></event-thumbnail>
+  </div>
+ `
+})
+export class EventsListComponent {
+  event1 = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '10/16/2021',
+    time: '10:00 am',
+    price: 599.99,
+    imageURL: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+  }
+}
