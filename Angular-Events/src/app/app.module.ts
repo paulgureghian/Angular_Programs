59b8688db4f76c3cea0ff3a3ebb0326a1c506757
paulgureghian <paulgureghian@att.net>
2020-12-30 14:49:38 -0800

    Modified file

--- Angular-Events/src/app/app.module.ts
+++ Angular-Events/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   bootstrap: [EventsAppComponent]
 })
