435a2a0e76d56d7fa7eb60f7e5fa2aaec05dafb4
paulgureghian <paulgureghian@att.net>
2020-12-30 14:56:43 -0800

    Modified file

--- Angular-Events/src/app/events-app.component.ts
+++ Angular-Events/src/app/events-app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: `
-    <h2>Hello Angular</h2>
-    <img src="/assets/images/basic-shield.png"/>
-  `  
+  template: '<events-list></events-list>'
 })
 export class EventsAppComponent {
   title = 'Angular-Events';
