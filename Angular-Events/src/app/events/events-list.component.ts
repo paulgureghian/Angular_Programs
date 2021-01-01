ee9ff79552ef24048eb10d61b1af23d241a78949
paulgureghian <paulgureghian@att.net>
2021-01-01 13:59:25 -0800

    Modified file

--- Angular-Events/src/app/events/events-list.component.ts
+++ Angular-Events/src/app/events/events-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core'
   <div>
     <h1>Upcoming Angular Events</h1>
     <hr />
+    <div class="well">
+      <div>For Angular developers</div>
+    </div>  
     <event-thumbnail [event]="event1"></event-thumbnail>
   </div>
  `
