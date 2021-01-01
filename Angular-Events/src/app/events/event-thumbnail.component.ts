9387f5e923e4d888baa88772e842aefc62cac384
paulgureghian <paulgureghian@att.net>
2021-01-01 13:50:18 -0800

    Modified file

--- Angular-Events/src/app/events/event-thumbnail.component.ts
+++ Angular-Events/src/app/events/event-thumbnail.component.ts
@@ -10,13 +10,15 @@ import { Component, Input } from '@angular/core'
     <div>Price: \${{event.price}}</div>
     <div>
       <span>Location: {{event.location.address}}</span>
-      <span>&nbsp;</span>
-      <span>{{event.location.city}}, {{event.location.country}}</span>
+      <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
     </div>
   </div>
- `
+ `,
+ styles: [`
+   .pad-left { margin-left: 10px; }
+   .well div { color: red; }
+ `]
 })
 export class EventThumbnailComponent {
   @Input() event:any
-
 }
