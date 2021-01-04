35c54ea89d97546730eafb2740eeb536008dce70
paulgureghian <paulgureghian@att.net>
2021-01-03 16:37:58 -0800

    Defined navbar component

--- Angular-Events/src/app/nav/navbar.component.ts
+++ Angular-Events/src/app/nav/navbar.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
-  templateUrl: './nav-bar.component.html'
+  templateUrl: './navbar.component.html',
+  styles: [`
+    .nav .navbar-nav {font-size: 15px;}
+    #searchForm {margin-right: 100px;}
+    @media (max-width: 1200px) {#searchForm {display: none}}
+  `]
 })
 export class NavBarComponent {
 
